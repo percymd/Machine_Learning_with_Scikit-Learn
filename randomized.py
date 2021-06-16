43b103eac102db25d8e3ae2ba4795db767324199
Percy Marquez <percymd10@gmail.com>
2021-06-16 17:02:42 -0500

    Randomized

--- randomized.py
+++ randomized.py
@@ -7,3 +7,20 @@ if __name__ == '__main__':
 
     dataset = pd.read_csv('./datasets/felicidad.csv')
     print(dataset)
+
+    X = dataset.drop(['country', 'rank', 'score'], axis=1)
+    y = dataset['score']
+
+    reg = RandomForestRegressor()
+
+    parametros = {
+        'n_estimators' : range(4,16),
+        'criterion' : ['mse', 'mae'],
+        'max_depth' : range(2,11)
+    }
+
+    rand_est = RandomizedSearchCV(reg, parametros, n_iter=10, cv=3, scoring='neg_mean_absolute_error').fit(X,y)
+
+    print(rand_est.best_estimator_)
+    print(rand_est.best_params_)
+    print(rand_est.predict(X.loc[[0]]))
